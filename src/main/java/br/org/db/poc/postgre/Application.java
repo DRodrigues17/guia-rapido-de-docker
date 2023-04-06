31278299c7c775508efd3e6a79346bbf7486e545
drodrigues17 <adanielluiz71@gmail.com>
2023-04-06 15:17:41 -0300

    guia de docker

---
+++ src/main/java/br/org/db/poc/postgre/Application.java
@@ -0,0 +1,13 @@
+package br.org.db.poc.postgre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/org/db/poc/postgre/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.db.poc.postgre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
